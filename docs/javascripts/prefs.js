d314a9a7391a5d413fff246d8ce3915d64a72cdb
mibracy <crusty512@yahoo.com>
2026-05-19 20:09:27 -0500

    Move style picker next to palette toggle, add page transition animation

--- docs/javascripts/prefs.js
+++ docs/javascripts/prefs.js
@@ -127,8 +127,8 @@ P.openModal = function () {
 };
 
 P.addButton = function () {
-  var header = document.querySelector(".md-header__inner");
-  if (!header) return;
+  var toggle = document.querySelector('.md-header__button[title*="Switch to"]');
+  if (!toggle) return;
 
   var btn = document.createElement("a");
   btn.className = "md-header__button md-icon";
@@ -136,7 +136,7 @@ P.addButton = function () {
   btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-8c0 1.1.9 2 2 2s2-.9 2-2-.9-2-2-2-2 .9-2 2zm4-2c0 .55.45 1 1 1s1-.45 1-1-.45-1-1-1-1 .45-1 1zm-6 0c0-.55-.45-1-1-1s-1 .45-1 1 .45 1 1 1 1-.45 1-1zm7.07 5.07l-1.41 1.41c-.39.39-.39 1.02 0 1.41s1.02.39 1.41 0l1.41-1.41c.39-.39.39-1.02 0-1.41s-1.02-.39-1.41 0z"/></svg>';
   btn.style.cursor = "pointer";
   btn.addEventListener("click", P.openModal);
-  header.appendChild(btn);
+  toggle.parentNode.insertBefore(btn, toggle.nextSibling);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -145,4 +145,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 document.addEventListener("DOMContentSwitch", function () {
   P.restore();
+  var el = document.querySelector(".md-content");
+  if (el) {
+    el.style.animation = "none";
+    requestAnimationFrame(function () { el.style.animation = ""; });
+  }
 });
